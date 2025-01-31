d6d26a8bcd8085291a51c73299848fa7c41a40ff
Clinton.G <clintongoin@gmail.com>
2025-01-31 15:40:45 -0600

    project

--- project/src/vite.config.js
+++ project/src/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig as defineVitestConfig } from 'vitest/config';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
+  build: {
+    outDir: 'build', 
   },
-});
+})
